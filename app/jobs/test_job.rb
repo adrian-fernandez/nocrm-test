096311dd6efb689b0dc53e97407e545d05d226ee
Adrián Fernández <jomerin@gmail.com>
2016-08-11 12:43:48 +0200

    Added folder to test results

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,6 +2,6 @@ class TestJob < ActiveJob::Base
   queue_as :default
  
   def perform(_repository_url, _project_name, _branch_name)
-    Git.new(_repository_url, _project_name, _branch_name)
+    g = Git.new(_repository_url, _project_name, _branch_name)
   end
 end
--- app/models/git.rb
+++ app/models/git.rb
@@ -1,13 +1,22 @@
 class Git
 
 	PROJECTS_PATH = "/tmp/projects"
+	TEST_RESULT_FOLDER = "test_result"
+
+	attr_accessor :local_path
 
 	def initialize(_repository_url, _project_name, _branch_name)
 		path = [PROJECTS_PATH, _project_name, _branch_name].join('/')
+		self.local_path = path
 		FileUtils.rm_rf(path) rescue nil
 		FileUtils.mkdir_p(path)
+		FileUtils.mkdir_p(get_output_path())
 
 		system("git clone -b #{_branch_name} #{_repository_url} #{path}")
+	end
+
+	def get_output_path
+		[self.local_path, TEST_RESULT_FOLDER].join('/')
 	end
 
 end
