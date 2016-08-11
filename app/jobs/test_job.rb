c1a5013492f2942eb4c02dd7a54fc7390c721220
Adrián Fernández <jomerin@gmail.com>
2016-08-11 12:58:05 +0200

    Added job for launch tests

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -3,5 +3,7 @@ class TestJob < ActiveJob::Base
  
   def perform(_repository_url, _project_name, _branch_name)
     g = Git.new(_repository_url, _project_name, _branch_name)
+
+    bundle exec rspec spec --format h > output.html
   end
 end
